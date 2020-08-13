e99da0989bd65e2050831beb4b3ba2a4361e919f
andrewwestgard <westgaar9629@uwec.edu>
2020-08-12 19:13:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import {BrowserRouter as Router, Switch, Route, Link }
+from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* header */}
+      <Header />
+      <Router>
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+      </Router>
+      {/* <Router>
+        <Switch>
+          <Header />
+        </Switch>
+      </Router> */}
+      {/* Tinder Cards */}
+        {/* Buttons below tinder cards */}
+
+        {/* Chat screen */}
+        {/* Individual chat screen */}
+        {/* Account screen */}
     </div>
   );
 }
